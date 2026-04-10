6846f423e3135f21f912c36e6f5a439202ad3995
Abhishek <abhishekkumre2001@gmail.com>
2026-04-10 13:21:25 +0530

    server.js change

--- server.js
+++ server.js
@@ -32,13 +32,15 @@ const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     httpOnly: true,
     sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000
   }
 });
-
+if (process.env.NODE_ENV === 'production') {
+  sessionConfig.cookie.secure = true;
+}
 app.use(sessionMiddleware);
 
 io.use((socket, next) => {
